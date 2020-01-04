45abbc317dd47681fe50f7f345d7235b21d20982
OIeg Ishenko <oleg.ishenko@divisionlab.com>
2020-01-04 14:24:40 +0100

    feat: user model database migration

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from config import config_list
 
@@ -23,6 +23,7 @@ else:
     raise EnvironmentError('Cannot find environment config')
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class: object = config_val):
     """Create and configure the instance of the application."""
@@ -30,6 +31,7 @@ def create_app(config_class: object = config_val):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Set up file logging
     if not os.path.exists('logs'):
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ Implements the configuration related objects.
 :copyright: © 2020 by the DivisionLab (haftungsbeschränkt).
 """
 from app import create_app
+from app.user.models import User
 
 app = create_app()
