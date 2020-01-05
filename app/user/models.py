3573c6c6aee66f88af9d820661b434044e0be57e
OIeg Ishenko <oleg.ishenko@divisionlab.com>
2020-01-05 20:02:41 +0100

    fix: adding the missing is_admin attribute

--- app/user/models.py
+++ app/user/models.py
@@ -1,5 +1,6 @@
 """Models for the User package."""
 
+from typing import Union
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from email_validator import validate_email, EmailNotValidError
@@ -18,6 +19,7 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self) -> str:
         """Generate a representation of the user model."""
@@ -30,4 +32,3 @@ class User(db.Model):
     def check_password(self, password) -> bool:
         """Check the user password."""
         return check_password_hash(self.password_hash, password)
-
