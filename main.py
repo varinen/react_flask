46e87ea07b0454c4a72c3f8c8182aefec8136140
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2020-01-09 14:20:03 +0100

    feat: db migration for the Note entity

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Implements the configuration related objects.
 """
 from app import create_app, cli
 from app.user.models import User
+from app.note.models import Note
 
 app = create_app()
 cli.register(app)
